86c45fd79076c351875386c8e0000fe8c9f51933
AndranikA <andranika93@gmail.com>
2025-12-02 21:40:30 +0400

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ logger.setLevel(logging.INFO)
 
 # Конфигурация
 class Config:
-    BOT_TOKEN = "7487925725:AAHzJyVWRG2fklT0hQvaXrq-Cawq9vzomEQ"  # os.getenv("BOT_TOKEN")
-    RENDER_APP_URL = "https://einstein-point-bot.onrender.com"  # os.getenv("RENDER_APP_URL")
+    BOT_TOKEN = "8288434073:AAFZcDkM1ifsXPE69J22XwErtI7NZesT2V0"  # os.getenv("BOT_TOKEN")
+    RENDER_APP_URL = "https://ludobot-lx4c.onrender.com"  # os.getenv("RENDER_APP_URL")
     ALLOWED_CHAT_IDS = [-1002157100033, -1002439723121, -1002248982019]  # @Family_Worlds | @Einstein_bot_test_2 | Group(Private): BOT TEST
     CREATOR = "@andranik_amrahyan"
     
@@ -505,4 +505,5 @@ def main():
             application.stop()
 
 if __name__ == "__main__":
+
     main()
